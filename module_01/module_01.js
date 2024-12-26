dc26359cee6f8efd5a2a55dfc37c716b42d1bcd2
nyn89025 <127371629+nyn89025@users.noreply.github.com>
2024-12-26 08:55:24 +0000

    containr config done

---
+++ module_01/module_01.js
@@ -0,0 +1,18 @@
+// const palindrome = () => {
+//     let name = prompt('Enter your name')
+//     let newString = ''
+ 
+//     for (let i = name.length - 1; i >= 0; i--){
+//        newString += name[i]
+//     }
+//     if(newString == name){
+//        return 'this string is palindrome'
+//     }
+//     else{
+//        return 'this string is not a palindrome'
+//     }
+//  }
+ 
+//  console.log(palindrome());
+
+console.log('nayan moradiya');
